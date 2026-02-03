84b292afd9523fcd49839e3bd9d034608e71bbb9
haliphax <haliphax@gmail.com>
2026-02-03 09:05:51 -0600

    ✅ fix router tests

--- src/app/router.test.ts
+++ src/app/router.test.ts
@@ -1,17 +1,25 @@
 import { describe, expect, it, vi } from "vitest";
 import Home from "./views/home.vue";
+import View from "./views/view.vue";
 
-const { mockCreateRouter, mockCreateHistory } = vi.hoisted(() => ({
-	mockCreateRouter: vi.fn(),
-	mockCreateHistory: vi.fn(() => "mockCreateHistory"),
+const { mockCreateRouter, mockCreateHistory, mockDefineComponent } = vi.hoisted(
+	() => ({
+		mockCreateRouter: vi.fn(),
+		mockCreateHistory: vi.fn(() => "mockCreateHistory"),
+		mockDefineComponent: vi.fn(),
+	}),
+);
+
+vi.mock("vue", () => ({
+	Component: "Component",
+	defineComponent: mockDefineComponent,
 }));
-
-vi.mock("vue", () => ({ Component: "Component" }));
 vi.mock("vue-router", () => ({
 	createRouter: mockCreateRouter,
 	createWebHashHistory: mockCreateHistory,
 }));
 vi.mock("./views/home.vue", () => ({ default: "Home" }));
+vi.mock("./views/view.vue", () => ({ default: "View" }));
 
 await import("./router");
 
@@ -33,5 +41,6 @@ describe("router", () => {
 		const components = routes.map((c) => c.component);
 
 		expect(components).toContain(Home);
+		expect(components).toContain(View);
 	});
 });
